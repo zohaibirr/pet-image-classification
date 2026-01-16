8d45b5f3d77c7f0e293170f3c2d22f733701de4b
Zohaib Shahid <z.shahid@cgiar.org>
2026-01-16 19:56:00 +0500

    Update train.py
    adjusting class weights

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+from sklearn.utils.class_weight import compute_class_weight
 from utils.config import *
 from utils.data_loader import get_data_generators
 from utils.model_builder import build_model
@@ -8,11 +9,22 @@ train_data, val_data, _ = get_data_generators(
 
 model = build_model()
 model.summary()
+classes = np.unique(train_data.classes)
+
+class_weights = compute_class_weight(
+    class_weight='balanced',
+    classes=classes,
+    y=train_data.classes
+)
+
+class_weights = dict(zip(classes, class_weights))
+print("Class Weights:", class_weights)
 
 history = model.fit(
     train_data,
     validation_data=val_data,
-    epochs=EPOCHS
+    epochs=EPOCHS,
+    class_weight=class_weights
 )
 
 model.save(MODEL_PATH)
